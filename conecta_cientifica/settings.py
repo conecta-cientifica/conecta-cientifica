cb15000b0b03a8775e86ffa59135f1e77e20f39f
gbeloso <gabrielbeloso62@gmail.com>
2023-08-30 21:23:01 -0300

    Configurada Secret key no arquivo .env para impedir que seja enviada ao repositório

--- conecta_cientifica/settings.py
+++ conecta_cientifica/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vx5(ely(*yrul#82t@7t-6ndw+^9(a1_0a4s+w=2$=2t41@ko%'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
