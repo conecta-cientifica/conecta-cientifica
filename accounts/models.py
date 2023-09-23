6e3fd3bc138483a4009af24c7380a1e5c00142f2
gbeloso <gabrielbeloso62@gmail.com>
2023-09-23 12:24:25 -0300

    Feature: Criados modelos de Perfil, Formacao e linhas de pesquisa

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class LinhaPesquisa(models.Model):
+    descricao = models.CharField(max_length=100, null=False, blank = False)
+
+class Profile(models.Model):
+    userID = models.OneToOneField(User, on_delete = models.CASCADE)
+    linhas = models.ManyToManyField(LinhaPesquisa)
+    #projects = models.ManyToManyField(Projects)
+
+class Formacao_titulacao(models.Model):
+    titulo = models.CharField(max_length=100)
+    curso = models.CharField(max_length=100, null=False, blank=False)
+    universidade = models.CharField(max_length=100, null=False, blank=False)
+    tese = models.CharField(max_length=200, blank=False, null=False)
+    grande_area = models.CharField(max_length=100)
+    area = models.CharField(max_length=100)
+    sub_area = models.CharField(max_length=100)
+    especialidade = models.CharField(max_length=100)
+    perfil = models.ForeignKey(Profile, on_delete=models.CASCADE) 
