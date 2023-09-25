94a918dee620d04952dc6360d9aeb28b01d2bbf3
gbeloso <gabrielbeloso62@gmail.com>
2023-09-25 11:35:23 -0300

    Refactor: Substituída base de dados para MySQL na nuve usando AWS

--- conecta_cientifica/settings.py
+++ conecta_cientifica/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'conecta_cientifica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'conectadb',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'conecta_cientifica',
         'USER': 'admin',
-        'PASSWORD': 'RQipGwfdBLtuWux83lpaCgq7kx0194KG',
-        'HOST': 'dpg-ck53nuei9prc73eo80gg-a.ohio-postgres.render.com',
-        'PORT': '5432',
+        'PASSWORD': '12345678',
+        'HOST': 'mysqlserver.chycvm7sglwj.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
