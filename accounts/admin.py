0c27be6b0481f10ff37f8cd1a8dd72f057250445
fernandabucheri <49451585+fernandabucheri@users.noreply.github.com>
2023-10-07 05:20:09 -0300

    SP3: Feature - Edição do perfil de usuário

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Education, UserProfile, ResearchArea, ResearchProject, Tag
 
-# Register your models here.
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    pass  
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass  
+
+@admin.register(ResearchArea)
+class ResearchAreaAdmin(admin.ModelAdmin):
+    pass  
+
+@admin.register(ResearchProject)
+class ResearchProjectAdmin(admin.ModelAdmin):
+    pass  
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
