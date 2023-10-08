abda946582803f70187be5a882f644e856c02bcc
Gabriela Verza <gabriela.verza@gmail.com>
2023-10-08 15:09:21 -0300

    Página de exibição de projetos
    - Template para cards de projetos
    - Página de exibição dos cards

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def projects_feed_view(request):
+    project_cards = [['Objeto 1', 'Objeto 2', 'Objeto 3'], ['Objeto 4', 'Objeto 5', 'Objeto 6']]
+    return render(request, "projects-feed.html", {'project_cards': project_cards})
+
+def project_page_view(request):
+    return render(request, "projects-page.html")
