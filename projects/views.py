15c56f1000d20865f9a3fb86e0afe640a637babb
Gabriela Verza <gabriela.verza@gmail.com>
2023-10-11 20:20:28 -0300

    Att branch

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def projects_feed_view(request):
-    project_cards = [['Objeto 1', 'Objeto 2', 'Objeto 3'], ['Objeto 4', 'Objeto 5', 'Objeto 6']]
+    project_cards = [['Objeto 1', 'Objeto 2', 'Objeto 3'], ['Objeto 4', 'Objeto 5', 'Objeto 6']] #matriz (3 colunas) devera ser preenchida com dados do banco
     return render(request, "projects-feed.html", {'project_cards': project_cards})
 
 def project_page_view(request):
