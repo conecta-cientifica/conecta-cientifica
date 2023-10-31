0001c50d28150cd0a56523995d69bea8b9dbd9f9
gbeloso <gabrielbeloso62@gmail.com>
2023-10-30 22:40:32 -0300

    Feature: Adicionado adapter Lattes

---
+++ accounts/lattes/database_connection.py
@@ -0,0 +1,24 @@
+import mysql.connector
+from queryerror import QueryException
+
+class DataBaseConn():
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+               host='18.188.240.61',
+                database='myslqserver_secundario',
+                user='admin',
+                password='12345678',
+                port=3306
+            )
+    def __del__(self):
+        self.conn.close()
+    
+    def execute_query(self, query):
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(query)
+            result = cursor.fetchall()
+            cursor.close()
+            return result
+        except:
+            raise(QueryException)
---
+++ accounts/lattes/profile.py
@@ -0,0 +1,6 @@
+class Profile():
+    def __init__(self):
+        self.researcher = None
+        self.titulations = None
+        self.projects = None
+        self.lines = None
