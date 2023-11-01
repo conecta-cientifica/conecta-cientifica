20b6f454a3d403df3530072371b9e412e94bc83a
fernandabucheri <49451585+fernandabucheri@users.noreply.github.com>
2023-11-01 15:54:34 -0300

    update app projects: feed, visualização de projetos e criação de projetos

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    advisor = models.CharField(max_length=100)
+    description = models.TextField()
+    requirements = models.TextField()
+
+    def __str__(self):
+        return self.title
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Project
+from .forms import ProjectForm
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url='/login/')
 def projects_feed_view(request):
-    project_cards = [['Objeto 1', 'Objeto 2', 'Objeto 3'], ['Objeto 4', 'Objeto 5', 'Objeto 6']] #matriz (3 colunas) devera ser preenchida com dados do banco
+    project_cards = Project.objects.all()  # Obtenha todos os projetos do banco de dados
     return render(request, "projects-feed.html", {'project_cards': project_cards})
 
-def project_page_view(request):
-    return render(request, "project-page.html")
+
+# # Exibe o projeto escolhido pelo usuário no feed - na url fica o id do projeto
+@login_required(login_url='/login/')
+def project_page_view(request, project_id):
+    print(f'PROJECT ID: {project_id}')  # Isso verificará se o ID do projeto está correto
+    project = get_object_or_404(Project, pk=project_id)
+    
+    print(f'Project Title: {project.title}')  # Isso imprimirá o título do projeto
+    print(f'Advisor: {project.advisor}')  # Isso imprimirá o professor orientador
+    print(f'Description: {project.description}')  # Isso imprimirá a descrição do projeto
+
+    # print('Requirements:')  # Isso imprimirá os requisitos do projeto
+    print(project.requirements)
+        
+    return render(request, "project-page.html", {'project': project})
+
+@login_required(login_url='/login/')
+def create_project(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects-feed')
+    else:
+        form = ProjectForm()
+    return render(request, 'create_project.html', {'form': form})
+
+@login_required(login_url='/login/')
+def edit_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects-feed')
+    else:
+        form = ProjectForm(instance=project)
+    return render(request, 'edit_project.html', {'form': form, 'project': project})
+
+@login_required(login_url='/login/')
+def delete_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects-feed')
+    return render(request, 'delete_project.html', {'project': project})
