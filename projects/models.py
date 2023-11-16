e036762fab520875ae0729ee46408bfd8ad3f5db
fernandabucheri <49451585+fernandabucheri@users.noreply.github.com>
2023-11-15 21:38:34 -0300

    add user que criou o projeto, pág de edição e exclusão de projeto

--- projects/models.py
+++ projects/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
     advisor = models.CharField(max_length=100)
     description = models.TextField()
     requirements = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True, blank=True)
 
     def __str__(self):
         return self.title
