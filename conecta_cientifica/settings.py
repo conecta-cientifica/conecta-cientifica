c5864b0143969ce5a1ef6ed3039cc4018f1cb4d6
fernandabucheri <49451585+fernandabucheri@users.noreply.github.com>
2023-11-18 13:23:23 -0300

    update bd

--- conecta_cientifica/settings.py
+++ conecta_cientifica/settings.py
@@ -91,23 +91,17 @@ WSGI_APPLICATION = 'conecta_cientifica.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'conecta_cientifica',
-#         'USER': 'admin',
-#         'PASSWORD': '12345678',
-#         'HOST': '18.116.128.60',
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'conecta_cientifica',
+        'USER': 'admin',
+        'PASSWORD': '12345678',
+        'HOST': '18.116.128.60',
+        'PORT': '3306',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
