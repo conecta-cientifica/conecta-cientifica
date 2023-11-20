6611a3b20e906285652212d5e99e68107f6e4dca
Arthur-so <arthursantos.oliveira001@gmail.com>
2023-11-20 19:21:32 -0300

    static

--- conecta_cientifica/settings.py
+++ conecta_cientifica/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = ['/var/www/static/']
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 #     "/var/www/static/",
