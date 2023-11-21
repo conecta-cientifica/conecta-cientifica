35dc91208cc382c4bd193fc10d61cd0969296e49
Arthur-so <arthursantos.oliveira001@gmail.com>
2023-11-21 08:36:23 -0300

    print static

--- conecta_cientifica/settings.py
+++ conecta_cientifica/settings.py
@@ -144,6 +144,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+print(STATIC_ROOT)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
