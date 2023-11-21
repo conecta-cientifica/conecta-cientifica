32e5b47ead53e0d1d8afefd06e2c6295a9e2475c
Arthur-so <arthursantos.oliveira001@gmail.com>
2023-11-21 09:11:04 -0300

    teste caminho settings

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import main_view
 from django.conf.urls.static import static
-from django.conf import settings
+from conecta_cientifica import settings
 urlpatterns = [
     path('', main_view, name='main')
 ] + static(settings.STATIC_ROOT)
