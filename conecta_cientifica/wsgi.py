0fb2bedba02eaa781ad8c27a3278e7da1dfd327d
Arthur-so <arthursantos.oliveira001@gmail.com>
2023-11-29 09:26:48 -0300

    update novamente

--- conecta_cientifica/wsgi.py
+++ conecta_cientifica/wsgi.py
@@ -11,10 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-with open('requirements.txt', 'r') as arquivo:
-    # Lê todo o conteúdo do arquivo e o imprime
-    conteudo = arquivo.read()
-    print(conteudo)
+# with open('requirements.txt', 'r') as arquivo:
+#     # Lê todo o conteúdo do arquivo e o imprime
+#     conteudo = arquivo.read()
+#     print(conteudo)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conecta_cientifica.settings')
 
