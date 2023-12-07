932733f7ba1e8b60dece75ec8fd22420270ad6d1
fernandabucheri <49451585+fernandabucheri@users.noreply.github.com>
2023-12-07 00:08:31 -0300

    add link usuários
    adição do link para redirecionar para a página do usuário que criou o projeto / que solicitou aprovação em um projeto.

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,9 +10,6 @@ urlpatterns = [
     path('user/<int:pk>/', views.user_detail, name='user_detail'),
     path('user/<int:pk>/update/', views.user_update, name='user_update'),
     path('user/<int:pk>/delete/', views.user_delete, name='user_delete'),
-
-    # path('user-profile/', views.user_profile_view, name='user-profile'),
-    # path('user-profile-edit/', views.user_profile_edit_view, name='user-profile-edit'),
     path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
